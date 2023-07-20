24521e008fcfb83eee14709fd2e1f5f9d26fc170
Baraa Basim <barabasim77@gmail.com>
2023-07-20 13:34:50 +0800

    play round

--- javascript.js
+++ javascript.js
@@ -1,9 +1,8 @@
 
 
-
 let choices = ['Rock', 'Paper', 'Scissors']
 
-function getComputerChoice(choices){
+function getComputerSelection(choices){
     let randomNumber;
     randomNumber = Math.floor(Math.random() * 3);
 
@@ -11,3 +10,20 @@ function getComputerChoice(choices){
 }
 
 
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase()
+    computerSelection = computerSelection.toLowerCase()
+    if (playerSelection == computerSelection){
+        return 0
+    } else if ((playerSelection == 'rock' && computerSelection == 'scissors')
+     || (playerSelection == 'scissors' && computerSelection == 'paper')
+     || (playerSelection == 'paper' && computerSelection == 'rock')) {
+        return 1
+    } else return 2;
+}
+
+const playerSelection = 'Paper'
+const computerSelection = getComputerSelection(choices);
+console.log(computerSelection)
+console.log(playerSelection)
+console.log(playRound(playerSelection, computerSelection));
